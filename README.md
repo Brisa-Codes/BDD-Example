b5785a462dfa38a9e523688cf7de9d031bb3cfa9
Brisa-Codes <brisamukunde1@gmail.com>
2022-06-17 22:21:41 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# bdd-example
+Behaviour Driven Development Example
+
+# Steps
+cd bdd-example-class
+
+
+# run tests
+npm test -- -p default -p report -p project
+
+# run APP
+npm run start - in production  
+npm run dev - in development  
---
+++ cucumber.js
@@ -0,0 +1,7 @@
+module.exports = {
+  default: '-f @cucumber/pretty-formatter --publish-quiet',
+  report: '--format html:./cucumber-report.html',
+  project: 'calcFeatures -r calcFeatures/calcSteps',
+  calc: 'calcFeatures/calc.feature -r calcFeatures/calcSteps',
+  gui: 'calcFeatures/gui.feature -r calcFeatures/calcSteps',
+};
